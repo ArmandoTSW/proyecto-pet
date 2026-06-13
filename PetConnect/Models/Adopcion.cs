477a2d401d88d0c112f5ca3ccfd90432c1966047
ArmandoTSW <jesus.cen@tecdesoftware.edu.mx>
2026-06-12 23:06:01 -0600

    feat: agregar modelos principales de PetConnect

---
+++ PetConnect/Models/Adopcion.cs
@@ -0,0 +1,10 @@
+﻿namespace PetConnect.Models;
+
+public class Adopcion
+{
+    public int Id { get; set; }
+    public int MascotaId { get; set; }
+    public int AdoptanteId { get; set; }
+    public DateTime Fecha { get; set; }
+    public string Estado { get; set; } = "";
+}
---
+++ PetConnect/Models/Adoptante.cs
@@ -0,0 +1,9 @@
+﻿namespace PetConnect.Models;
+
+public class Adoptante
+{
+    public int Id { get; set; }
+    public string Nombre { get; set; } = "";
+    public string Telefono { get; set; } = "";
+    public string Correo { get; set; } = "";
+}
